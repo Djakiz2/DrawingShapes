31647d4704daa069efd27ad860cd2b9725ab5f93
Djakiz2 <djaki.nikola@gmail.com>
2025-05-20 12:46:28 +0200

    Random changes - testing git

--- src/geometry/Shape.java
+++ src/geometry/Shape.java
@@ -5,9 +5,11 @@ import java.awt.Graphics;
 public abstract class Shape {
 	protected boolean selected;
 	
+	
 	public Shape() {
 		
 	}
+	
 	
 	public Shape(boolean selected) {
 		this.selected = selected;
@@ -24,4 +26,14 @@ public abstract class Shape {
 	public abstract boolean contains(int x, int y);
 	public abstract void draw(Graphics g);
 	
+	public static void someFunc() {
+		System.out.print("RADI");
+	}
+	
 }
+
+
+
+
+
+
